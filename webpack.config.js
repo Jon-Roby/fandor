81918a6afaefcc59845d39b90ea65fd7b5a68f7b
Jon Roby <jonathan.roby1@gmail.com>
2016-10-03 01:04:02 -0700

    remove comments in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -41,23 +41,3 @@ module.exports = {
     },
   },
 };
-
-// var path = require('path');
-//
-// const loaderConfig = [
-//   {
-//     test: /\.js$/,
-//     loaders: ['babel'],
-//     include: path.join(__dirname, 'src')
-//   },
-//   {
-//     test: /\.scss$/,
-//     loaders: ['style', 'css?module&localIdentName=[local]---[hash:base64:5]', 'postcss', 'sass']
-//   },
-//   {
-//     test: /\.(png|jpg)$/,
-//     loader: 'url-loader?limit=8192'
-//   }
-// ];
-//
-// module.exports = loaderConfig;
